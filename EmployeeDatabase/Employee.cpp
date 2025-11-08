109d2efbf696aa7e7ce9ce2691e4f5c89229c6c9
Gen346 <kelzreez@gmail.com>
2025-11-09 01:27:41 +0200

    Added getters, overloaded operator, and methods for employees save

--- EmployeeDatabase/Employee.cpp
+++ EmployeeDatabase/Employee.cpp
@@ -13,3 +13,25 @@ Employee::Employee(int salary, std::string firstName, std::string lastName)
 	mFirstName = firstName;
 	mLastName = lastName;
 }
+std::string Employee::getFirstName() const
+{
+	return mFirstName;
+}
+
+std::string Employee::getSecondName() const
+{
+	return mLastName;
+}
+
+int Employee::getSalary() const
+{
+	return mSalary;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Employee& emp)
+{
+	os << "Name: " << emp.getFirstName() << " " << emp.getSecondName()
+		<< ", Salary: " << emp.getSalary() << "\n";
+	return os;
+}
